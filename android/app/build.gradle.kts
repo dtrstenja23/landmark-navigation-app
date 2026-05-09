f164090ca90257b0a2fe5ea789a754b6563fad33
dtrstenja23 <dtrstenja23@student.foi.hr>
2026-05-09 15:47:11 +0200

    Add current location retrieveal #1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (localPropertiesFile.exists()) {
 android {
     namespace = "com.example.landmark_navigation_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,11 +11,52 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late GoogleMapController mapController;
+  bool _locationLoaded = false;
+  LatLng _center = const LatLng(45.8150, 15.9819); // Zagreb
 
-  final LatLng _center = const LatLng(45.8150, 15.9819); // Zagreb
+  @override
+  void dispose() {
+    mapController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserLocation();
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    if (_locationLoaded) {
+      controller.animateCamera(CameraUpdate.newLatLngZoom(_center, 25));
+    }
+  }
+
+  Future<void> _loadUserLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return;
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return;
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+    final userLatLng = LatLng(position.latitude, position.longitude);
+    mapController.animateCamera(CameraUpdate.newLatLng(userLatLng));
+    setState(() {
+      _locationLoaded = true;
+      _center = userLatLng;
+    });
   }
 
   @override
@@ -27,6 +69,8 @@ class _HomeScreenState extends State<HomeScreen> {
       body: GoogleMap(
         onMapCreated: _onMapCreated,
         initialCameraPosition: CameraPosition(target: _center, zoom: 12.0),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
       ),
     );
   }
