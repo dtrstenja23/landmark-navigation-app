dff3292f2dddcadb57b40aba70936e30ade6249a
dtrstenja23 <dtrstenja23@student.foi.hr>
2026-06-25 12:44:02 +0200

    remove Firebase dependencies and configurations

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:landmark_navigation_app/screens/home_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: MyApp()));
 }
 
